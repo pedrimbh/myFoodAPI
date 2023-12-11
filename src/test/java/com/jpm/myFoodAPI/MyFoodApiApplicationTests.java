3abf5d1b80cb12a58bc01ede1c2ecb3496dbf8f8
Joao Pedro Mesquita <joaopmesquita92@gmail.com>
2023-12-11 00:22:15 -0300

    Initial commit

---
+++ src/test/java/com/jpm/myFoodAPI/MyFoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpm.myFoodAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
